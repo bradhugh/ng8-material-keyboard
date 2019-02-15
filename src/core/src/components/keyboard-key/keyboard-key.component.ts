e2a79dea335e5b1addbf2116f1030b0031c7f316
Brad Hughes <bradhugh@microsoft.com>
2019-02-15 15:25:38 -0500

    Fix bug when binding to numbers

--- src/core/src/components/keyboard-key/keyboard-key.component.ts
+++ src/core/src/components/keyboard-key/keyboard-key.component.ts
@@ -222,7 +222,7 @@ export class MatKeyboardKeyComponent implements OnInit {
   }
 
   private deleteSelectedText(): void {
-    const value = this.inputValue ? this.inputValue : "";
+    const value = this.inputValue == null ? "" : this.inputValue.toString();
     let caret = this.input ? this._getCursorPosition() : 0;
     let selectionLength = this._getSelectionLength();
     if (selectionLength === 0) {
